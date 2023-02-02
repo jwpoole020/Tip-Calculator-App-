9befed2bf7f00d682c88bf8919b783922094c0ee
android <asdf>
2023-02-02 11:24:31 -0500

    first save

---
+++ app/src/main/java/com/example/tipcalculatorapp/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.tipcalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText purchasePrice;
+    EditText numPeople;
+    SeekBar seekBar;
+    RadioGroup radioGroup;
+    RadioButton radioEntire;
+    RadioButton radioSplit;
+    TextView tipText;
+    TextView totalPrice;
+    double tip=.15;
+    double total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        purchasePrice = findViewById(R.id.purchasePrice);
+        numPeople = findViewById(R.id.numPeople);
+        seekBar = findViewById(R.id.seekBar);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioEntire = findViewById(R.id.radioEntire);
+        radioSplit = findViewById(R.id.radioSplit);
+        tipText = findViewById(R.id.tipText);
+        totalPrice = findViewById(R.id.totalPrice);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tipText.setText("%"+i);
+                tip=i;
+                tip=tip/100;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        purchasePrice.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    double value = Double.parseDouble(purchasePrice.getText().toString());
+                    if (radioEntire.isChecked()) {
+                        total = value + (value * tip);
+
+                    } else if (radioSplit.isChecked()) {
+                        double people = Double.parseDouble(numPeople.getText().toString());
+                        total = (value + (value * tip)) / people;
+
+                    }
+                    totalPrice.setText("$" + String.format("%.2f", total));
+                }
+
+                return false;
+            }
+        });
+        numPeople.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    double value = Double.parseDouble(purchasePrice.getText().toString());
+                    if (radioEntire.isChecked()) {
+                        total = value + (value * tip);
+
+                    } else if (radioSplit.isChecked()) {
+                        double people = Double.parseDouble(numPeople.getText().toString());
+                        total = (value + (value * tip)) / people;
+
+                    }
+                    totalPrice.setText("$" + String.format("%.2f", total));
+                }
+                return false;
+            }
+        });
+    }
+}
